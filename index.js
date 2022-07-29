a7b7cfd110b96abf5c295ab901616f46ddbf14b4
Gustavo Rodrigues <gstvrrds@gmail.com>
2022-07-29 00:16:29 -0300

    Colocando Informações no menu lateral mobile

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var menulateral = document.querySelector(".menu-lateral");
 botaomenu.addEventListener("click",()=>{
     menulateral.classList.toggle("menu-lateral-ativo")
 })
+var botao_x = document.querySelector(".Sair-menu")
+botao_x.addEventListener("click",()=>{
+    menulateral.classList.remove("menu-lateral-ativo")
+})
