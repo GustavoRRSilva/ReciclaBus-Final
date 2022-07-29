587978ffca4194965113ccb30eb79ccaa565eb42
Gustavo Rodrigues <gstvrrds@gmail.com>
2022-07-28 23:01:28 -0300

    adicionando arquivos iniciais

---
+++ index.js
@@ -0,0 +1,5 @@
+var botaomenu = document.querySelector(".icone-menu");
+var menulateral = document.querySelector(".menu-lateral");
+botaomenu.addEventListener("click",()=>{
+    menulateral.classList.toggle("menu-lateral-ativo")
+})
